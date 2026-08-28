0e436ce457d68c3ea950a17ed23432003a09e67b
Isabela Baldim <Isabela.Baldim2@nscorp.com>
2026-08-28 11:09:35 -0300

    chore: configure Sass and Git ignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main className="app">
@@ -13,8 +8,8 @@ function App() {
         <span className='game-title'>Questwood</span>
         <h1>Your adventure begins here</h1>
         <p>Complete small quests, earn rewards and help your little world to grow</p>
-        <button type='button'>
-          Begin today's quests.
+        <button className="primary-button" type="button">
+          Begin today's quests
         </button>
       </section>
     </main>
